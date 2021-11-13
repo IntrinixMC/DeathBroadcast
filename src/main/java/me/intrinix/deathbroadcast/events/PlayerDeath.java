30ff08d0248b51d79fd0049615680e9ff7e85c68
IntrinixMC <intrinix@onmail.com>
2021-11-13 19:22:35 +0545

    Added multiple broadcast message support

--- src/main/java/me/intrinix/deathbroadcast/events/PlayerDeath.java
+++ src/main/java/me/intrinix/deathbroadcast/events/PlayerDeath.java
@@ -8,35 +8,33 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import java.util.List;
+import java.util.Random;
+
 public class PlayerDeath implements Listener {
 
     DeathBroadcast plugin;
-
+    Random random = new Random();
     public PlayerDeath(DeathBroadcast plugin) {
         this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event){
+    public void onPlayerDeath(PlayerDeathEvent event) {
         final String PREFIX = "[Server]";
         Player player = event.getEntity().getPlayer();
         assert player != null;
         String playerName = player.getDisplayName();
         String defaultBroadcastMsg = ChatColor.translateAlternateColorCodes('&', "&4&lF");
-        String deathBroadcastMsg = plugin.getConfig().getString("death-broadcast-message");
-        if(deathBroadcastMsg != null){
-            deathBroadcastMsg = ChatColor.translateAlternateColorCodes('&', deathBroadcastMsg);
-            if(deathBroadcastMsg.contains("%player%")){
-                deathBroadcastMsg = deathBroadcastMsg.replaceAll("%player%", playerName);
-            }
-            if(deathBroadcastMsg.isEmpty()){
-                Bukkit.broadcastMessage(PREFIX + " " + defaultBroadcastMsg);
-            } else {
-                Bukkit.broadcastMessage(PREFIX + " " + deathBroadcastMsg);
-            }
+        List<String> deathBroadcastMsg = plugin.getConfig().getStringList("death-broadcast-message");
+        int msgIndex = random.nextInt(deathBroadcastMsg.size());
+        String newBroadcastMsg = ChatColor.translateAlternateColorCodes('&', deathBroadcastMsg.get(msgIndex).replaceAll("%player%", playerName));
+        if (!deathBroadcastMsg.isEmpty()) {
+            Bukkit.broadcastMessage(PREFIX + " " + newBroadcastMsg);
+        } else {
+            Bukkit.broadcastMessage(PREFIX + " " + defaultBroadcastMsg);
         }
-
 
     }
 
